ee15213e3f76e124e14ef48b33c873f29da00e86
plutach <jananadi92@gmail.com>
2019-11-20 00:54:53 +0900

    Create SubActivity02.java

---
+++ SubActivity02.java
@@ -0,0 +1,112 @@
+package com.example.image.pcs;
+
+import java.io.IOException;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.widget.Button;
+import android.view.View;
+import android.media.MediaPlayer;
+
+
+public class SubActivity02 extends AppCompatActivity {
+
+    private static MediaRecorder mediaRecorder;
+    private static MediaPlayer mediaPlayer;
+
+    private static String audioFilePath;
+    private static Button stopButton;
+    private static Button playButton;
+    private static Button recordButton;
+
+    private boolean isRecording = false;
+
+    protected boolean hasMicrophone() {
+        PackageManager pmanager = this.getPackageManager();
+        return  pmanager.hasSystemFeature(PackageManager.FEATURE_MICROPHONE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub02);
+
+        recordButton = (Button) findViewById(R.id.recordButton);
+        playButton = (Button) findViewById(R.id.playButton);
+        stopButton = (Button) findViewById(R.id.stopButton);
+
+        if (!hasMicrophone())
+        {
+            stopButton.setEnabled(false);
+            playButton.setEnabled(false);
+            stopButton.setEnabled(false);
+        }
+        else {
+            playButton.setEnabled(false);
+            stopButton.setEnabled(false);
+        }
+
+        audioFilePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/myaudio.3gp";
+    }
+
+    public void recordAudio (View view) throws  IOException
+    {
+        isRecording = true;
+        stopButton.setEnabled(true);
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+
+        try {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setOutputFile(audioFilePath);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.prepare();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        mediaRecorder.start();
+    }
+
+    public void stopAudio (View view)
+    {
+        stopButton.setEnabled(false);
+        playButton.setEnabled(true);
+
+        if (isRecording)
+        {
+            recordButton.setEnabled(false);
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            isRecording = false;
+        }
+        else
+        {
+            mediaPlayer.release();
+            mediaPlayer = null;
+            recordButton.setEnabled(true);
+        }
+    }
+
+    public void playAudio (View view) throws IOException
+    {
+        playButton.setEnabled(false);
+        recordButton.setEnabled(false);
+        stopButton.setEnabled(true);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setDataSource(audioFilePath);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+    }
+
+}
+
+
